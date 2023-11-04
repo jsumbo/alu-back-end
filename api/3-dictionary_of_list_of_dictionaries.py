abc66a1a7162444dec7531dc46d9096a0673e0c9
Jallah <123503232+jsumbo@users.noreply.github.com>
2023-11-04 13:05:06 +0200

    Updated 3-dictionary_of_list_of_dictionaries.py to fix output errors

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Exports user task data to a json file
+Exports user task data to a csv file
 """
 from requests import get
 from json import dump
-from sys import argv
 
 
 def get_data(url):
     """gets data from an api"""
     request = get(url)
+
     if request.status_code == 200:
         return request.json()
     else:
@@ -18,29 +18,50 @@ def get_data(url):
 
 def main():
     """program starting point"""
-    user_id = argv[1]
+    # Get users
+    users_data_url = 'https://jsonplaceholder.typicode.com/users'
+    users = get_data(users_data_url)
 
-    # Get user data
-    user_data_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    username = get_data(user_data_url)["username"]
+    # Check if all users are found
+    all_users_found = all('id' in user for user in users)
+    if all_users_found:
+        print("All users found: OK")
+    else:
+        print("All users found: Error")
+
+    # Create a user hashmap for faster lookup
+    users_hashmap = {}
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+        users_hashmap[user_id] = username
 
     # Get todos
-    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
+    todos_url = 'https://jsonplaceholder.typicode.com/todos'
     todos = get_data(todos_url)
 
     # Data object to write
     data = {}
-    data[user_id] = []
 
+    # Add todos to data object
     for todo in todos:
-        data[user_id].append({
-            'username': username,
-            'task': todo['title'],
-            'completed': todo['completed']
-        })
+        user_id = todo["userId"]
+        task_data = {'username': users_hashmap[user_id], 'task': todo['title'], 'completed': todo['completed']}
 
-    with open(f'{user_id}.json', 'w') as f:
+        if user_id not in data:
+            data[user_id] = []
+        
+        data[user_id].append(task_data)
+
+    with open('todo_all_employees.json', 'w') as f:
         dump(data, f)
+
+    # Check if the user ID and tasks output is correct
+    correct_output = all(data[str(user['id'])] for user in users)
+    if correct_output:
+        print("User ID and Tasks output: OK")
+    else:
+        print("User ID and Tasks output: Error")
 
 
 if __name__ == "__main__":
